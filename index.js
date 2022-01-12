c30eae26cf150196a6ba1cbff33a45ec4a720b53
Jesse Tane <jesse.tane@gmail.com>
2022-01-12 13:58:33 -0500

    better variable names

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ wsServer.on('connection', (client, req) => {
   console.log('connect', req.socket.remoteAddress)
   function onmessage (message) {
     if (client.authenticated) {
-      udpSocketSend.send(message, process.env.UDP_PORT_SEND || '1457', udpHostSend)
+      udpSendSocket.send(message, process.env.UDP_PORT_SEND || '1457', udpSendHost)
     } else {
       if (message.toString() === (process.env.PRE_SHARED_KEY || 'secret')) {
         client.authenticated = true
@@ -49,13 +49,13 @@ wsServer.on('connection', (client, req) => {
   }
 })
 
-const udpHostSend = process.env.UDP_HOST_SEND || '127.0.0.1'
-const udpSocketSend = dgram.createSocket(udpHostSend.indexOf(':') !== -1 ? 'udp6' : 'udp4')
+const udpSendHost = process.env.UDP_SEND_HOST || '127.0.0.1'
+const udpSendSocket = dgram.createSocket(udpSendHost.indexOf(':') !== -1 ? 'udp6' : 'udp4')
 
-const udpHostRecv = process.env.UDP_HOST_RECV || '255.255.255.255'
-const udpSocketRecv = dgram.createSocket(udpHostRecv.indexOf(':') !== -1 ? 'udp6' : 'udp4')
-udpSocketRecv.bind(process.env.UDP_PORT_RECV || '1457', udpHostRecv)
-udpSocketRecv.on('message', message => {
+const udpRecvHost = process.env.UDP_RECV_HOST || '255.255.255.255'
+const udpRecvSocket = dgram.createSocket(udpRecvHost.indexOf(':') !== -1 ? 'udp6' : 'udp4')
+udpRecvSocket.bind(process.env.UDP_RECV_PORT || '1457', udpRecvHost)
+udpRecvSocket.on('message', message => {
   wsClients.forEach(c => {
     if (c.authenticated) {
       c.send(message.toString())
